11a8c6a64346d2756d4a42865c822e6be0417f69
Taylor Woodcock <taylor.woodcock@outlook.com>
2022-03-13 23:05:09 +0000

    feat: Added some sequences - This just adds some simple init sound sequences

---
+++ sequence.go
@@ -0,0 +1,39 @@
+package main
+
+var TripleBeep = [][]int{
+	// notes
+	{
+		60,
+		64,
+		67,
+		0,
+	},
+	// timings
+	{
+		100,
+		100,
+		500,
+		0,
+	},
+}
+
+var TripleBeepFunky = [][]int{
+	// notes
+	{
+		60,
+		0,
+		60,
+		0,
+		72,
+		0,
+	},
+	// timings
+	{
+		100,
+		100,
+		150,
+		50,
+		500,
+		0,
+	},
+}
