752372cc58728d950ee0bde5c7e40885073dc432
Taylor Woodcock <taylor.woodcock@outlook.com>
2022-03-13 22:44:48 +0000

    feat: Added `playSequence` util - Allows you to play a sequence of notes and timings
    refactor: Moved some functions around
    
    refactor: Removed `midiMap` param from `randomBeeps`
    - This calculates the midiMap when called

--- utils.go
+++ utils.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"strconv"
 	"strings"
+	"time"
 
 	"gitlab.com/gomidi/midi"
 )
 
+// must checks if err != nil and panics if not nil
+func must(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+// remove removes a value from a slice of ints and returns its index
+func remove(slice []int, remove int) ([]int, int) {
+	for i, v := range slice {
+		if v == remove {
+			return append(slice[:i], slice[i+1:]...), i
+		}
+	}
+	return slice, 0
+}
+
 // decodeMidi decodes a midi.Message into a Midi struct
 func decodeMidi(msg midi.Message) (*Midi, error) {
 	parts := strings.Split(msg.String(), " ")
-	fmt.Printf("Parts: %#v\n", parts)
+	// fmt.Printf("Parts: %#v\n", parts)
 
 	var (
 		channel  string
@@ -75,6 +94,33 @@ func decodeMidi(msg midi.Message) (*Midi, error) {
 	return midi, nil
 }
 
+// playSequence iterates a slice of notes and timings
+func playSequence(b Beeper, seqNotes []int, seqTimes []int) error {
+	if len(seqNotes) != len(seqTimes) {
+		return errors.New("seqNotes and seqTimes length mismatch")
+	}
+
+	for i, n := range seqNotes {
+		if n == 0 {
+			// TODO (TW) protect against first val being 0
+			b.NoBeep(seqNotes[i-1])
+			return nil
+		}
+		err := b.Beep(n, BendZero)
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(time.Millisecond * time.Duration(seqTimes[i]))
+
+		if i > 0 {
+			b.NoBeep(seqNotes[i-1])
+		}
+	}
+
+	return nil
+}
+
 // calculateMidiFrequencies calculates the frequencies for each midi value from min to max
 func calculateMidiFrequencies(min, max int) map[int]float64 {
 	midiMap := make(map[int]float64)
@@ -85,30 +131,17 @@ func calculateMidiFrequencies(min, max int) map[int]float64 {
 }
 
 // randomBeeps iterates a map of midi keys and plays the keys in the randomised map order through the Beeper
-func randomBeeps(beeper Beeper, midiMap map[int]float64) error {
+func randomBeeps(beeper Beeper) error {
+	midiMap := calculateMidiFrequencies(0, 255)
 	for k := range midiMap {
 		err := beeper.Beep(k, 0)
 		if err != nil {
 			return err
 		}
-	}
-	beeper.NoBeep()
-	return nil
-}
-
-// must checks if err != nil and panics if not nil
-func must(err error) {
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
-// remove removes a value from a slice of ints and returns its index
-func remove(slice []int, remove int) ([]int, int) {
-	for i, v := range slice {
-		if v == remove {
-			return append(slice[:i], slice[i+1:]...), i
+		err = beeper.NoBeep(k)
+		if err != nil {
+			return err
 		}
 	}
-	return slice, 0
+	return nil
 }
